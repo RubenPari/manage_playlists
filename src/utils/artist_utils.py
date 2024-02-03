de37603152103be5569d0f255ef4cd0220c14061
RubenPari <rubenpari.developer@gmail.com>
2024-02-03 11:28:26 +0100

    fixed import

--- src/utils/artist_utils.py
+++ src/utils/artist_utils.py
@@ -1,10 +1,10 @@
-from ..server import spotify_api
+from ..main import spotify_api
 
 def get_first_artist_genre(artist_id):
     artist_info = spotify_api.artist(artist_id)
-    
+
     genres = artist_info.get('genres', [])
-    
+
     if genres:
         return genres[0]
     else:
